ee86dd4030383372dd96d5308c53f86503f7395d
Majkl <majkl.andracic@gmail.com>
2020-02-29 23:59:09 +0100

    Create order

--- app/src/main/java/com/example/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/OrderActivity.java
@@ -3,6 +3,7 @@ package com.example.bitsandpizzas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
