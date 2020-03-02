a933c7f560a0af778608f2af141f45d1803a3e90
Majkl <majkl.andracic@gmail.com>
2020-03-02 22:16:20 +0100

    Adding up button

--- app/src/main/java/com/example/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/OrderActivity.java
@@ -1,7 +1,7 @@
 package com.example.bitsandpizzas;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 
@@ -13,5 +13,7 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
     }
 }
