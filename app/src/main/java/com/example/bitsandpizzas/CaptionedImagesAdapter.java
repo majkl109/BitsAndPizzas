9bdc932a7b181781daf2049637532078333c756f
Majkl <majkl.andracic@gmail.com>
2020-05-22 23:36:50 +0200

    RecyclerView in progress

---
+++ app/src/main/java/com/example/bitsandpizzas/CaptionedImagesAdapter.java
@@ -0,0 +1,25 @@
+package com.example.bitsandpizzas;
+
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
+
+    private String[] captions;
+    private int[] imageIds;
+    private Listener listener;
+
+    interface Listener{
+        void onClick(int position);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView cardView;
+
+        public ViewHolder(CardView v){
+            super(v);
+            cardView = v;
+        }
+    }
+}
