7bcc2852f38613d74e7a0b0df4ffc86ae725f697
Majkl <majkl.andracic@gmail.com>
2020-05-29 21:04:50 +0200

    Card view in progress

--- app/src/main/java/com/example/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,7 +1,14 @@
 package com.example.bitsandpizzas;
 
 
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
@@ -21,5 +28,36 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
             super(v);
             cardView = v;
         }
+    }
+
+    public CaptionedImagesAdapter(String[] captions, int[] imageIds){
+        this.captions = captions;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public int getItemCount(){
+        return captions.length;
+    }
+
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType){
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_captioned_image,parent,false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        CardView cardView = holder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.info_image);
+        Drawable drawable =
+                ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(captions[position]);
+        TextView textView = cardView.findViewById(R.id.info_text);
+        textView.setText(captions[position]);
+
     }
 }
