3d0685251ad90fe26a3617f505199d864c81e189
Majkl <majkl.andracic@gmail.com>
2020-06-09 13:34:32 +0200

    Setting action bar and pizza details

--- app/src/main/java/com/example/bitsandpizzas/PizzaDetailActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/PizzaDetailActivity.java
@@ -1,14 +1,35 @@
 package com.example.bitsandpizzas;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PizzaDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_PIZZA_ID = "pizzaId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+
+        int pizzaId = (Integer)getIntent().getExtras().get(EXTRA_PIZZA_ID);
+        String pizzaName = Pizza.pizzas[pizzaId].getName();
+        TextView textView = findViewById(R.id.pizza_text);
+        textView.setText(pizzaName);
+        int pizzaImage = Pizza.pizzas[pizzaId].getImageResourceId();
+        ImageView imageView = findViewById(R.id.pizza_image);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, pizzaImage));
+        imageView.setContentDescription(pizzaName);
     }
 }
